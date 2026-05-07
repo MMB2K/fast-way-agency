efa19c94516042f97394c943729d628f35f6ccaf
mmb2k <mounirmoctar@gmail.com>
2026-05-07 09:02:58 +0000

    Remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import path from 'path';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-
+base: '/fast-way-agency/',
   plugins: [
     TanStackRouterVite(),
     react(),
